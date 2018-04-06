f25f3551dafd794a9dd32dc78b7cc685cc558e5a
Ubuntu <e20161631@sogang.ac.kr>
2018-04-06 05:08:34 +0000

    #12. 상품 목록 페이지 구현

--- app/controllers/product_controller.rb
+++ app/controllers/product_controller.rb
@@ -1,8 +1,17 @@
 class ProductController < ApplicationController
-  def list
-    @vsellers = Vseller.all
-    
-  end
+   def list
+      page_id = params[:page_id].to_i
+      if page_id == 0
+         # exception handling
+      end
+      startpoint = ( (page_id -1) * 12 )
+      @vproducts = Vprodcut.order(:created_at).limit(12).offset(startpoint)
+      
+      @bills = { }
+      @vproducts.each do |vproduct|
+         @bills[vproduct.id] = Bill.find(vproduct.bill_id)
+      end 
+   end
 
   def detail
   end
