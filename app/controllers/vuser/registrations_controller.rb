de530483afd05097b01a2834806dee4b23a50a75
Ubuntu <e20161631@sogang.ac.kr>
2018-04-07 05:41:42 +0000

    #30. 회원가입 기능 커스터마이징 완료

--- app/controllers/vuser/registrations_controller.rb
+++ app/controllers/vuser/registrations_controller.rb
@@ -42,7 +42,7 @@ class Vuser::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
    def configure_sign_up_params
-     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :address, :account])
+     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :address, :account, :phone_number, :account_assos])
    end
 
   # If you have extra params to permit, append them to the sanitizer.
