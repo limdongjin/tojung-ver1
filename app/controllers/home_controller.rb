f3a555d0d17c8a343b126e22c265708536578391
Ubuntu <e20161631@sogang.ac.kr>
2018-04-11 06:56:12 +0000

    #43. 추가정보 받도록 설정

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,13 @@
 class HomeController < ApplicationController
   def index
    if !(current_vuser.blank?)
-      if current_vuser.real_name == nil or current_vuser.address == nil or current_vuser.phone_number == nil
+      if current_vuser.real_name == nil or 
+		  current_vuser.address == nil or 
+		  current_vuser.phone_number == nil or 
+		  current_vuser.real_name == "" or 
+		  current_vuser.phone_number == "" or
+		  current_vuser.address == ""
+
 		 redirect_to(edit_vuser_registration_path)
 	  end
    end
