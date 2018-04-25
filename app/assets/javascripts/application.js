0c886568484286d1d9ef819083e15d1ac2953eaa
Zinyon <jy2045@sogang.ac.kr>
2018-04-25 12:12:29 +0900

    #4. TypeWriter
    ddddd

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -31,19 +31,6 @@ function go(cid){
      }
 
 
-// Typewriter ( Main PAGE)
-var app = document.getElementById('mention');
-
-var typewriter = new Typewriter(app, {
-    loop: true
-});
-
-typewriter.typeString('겪고 있는 문제가 있나요?\n')
-    .pauseFor(1000)
-    .typeString('지금 바로 말해주세요. 당신의 투정.')
-    .pauseFor(2500)
-    .start();
-
 
 var abb= $(".alert");
 abb.hide(3000);
