1dc6f9d363e4855b379948785189d2acad8cec74
limdongjin <30315743+limdongjin@users.noreply.github.com>
2018-05-16 19:30:37 +0900

    의원 찬반 메일 구현 완료!!!!! ?!

--- app/controllers/response_controller.rb
+++ app/controllers/response_controller.rb
@@ -1,19 +1,33 @@
 class ResponseController < ApplicationController
+  def index
+    #if current_vuser != nil and current_vuser.email == "admin@2jung.com"
+  end
+
   def agree
     key = params[:key]
-    if !(PersonResponse.where(agree_hash: key).count.zero)?
+    print("key\n")
+
+    print(key)
+    print(key.class)
+    if PersonResponse.where(agree_hash: key).count != 0
       pr = PersonResponse.where(agree_hash: key)[0]
-      pr.response_type = '찬성'
+      print(pr)
+      pr.response_type = "찬성"
       pr.save
       return
     end
+
+    print("\nnono")
   end
 
   def disagree
-    if !(PersonResponse.where(disagree_hash: key).count.zero)?
+    key = params[:key]
+
+    if PersonResponse.where(disagree_hash: key).count != 0
       pr = PersonResponse.where(disagree_hash: key)[0]
-      pr.response_type = '반대'
+      pr.response_type = "반대"
       pr.save
+      return
     end
   end
 
