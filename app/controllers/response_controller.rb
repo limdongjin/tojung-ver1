ba9d7bd85907c63037f92431544483d739fdbf84
limdongjin <30315743+limdongjin@users.noreply.github.com>
2018-05-23 21:49:28 +0900

    이메일기능 개선(5)

--- app/controllers/response_controller.rb
+++ app/controllers/response_controller.rb
@@ -11,6 +11,7 @@ class ResponseController < ApplicationController
     print(key.class)
     @key = key
     @pr = ""
+
     if PersonResponse.where(agree_hash: key).count != 0
       pr = PersonResponse.where(agree_hash: key)[0]
       print(pr)
@@ -18,7 +19,8 @@ class ResponseController < ApplicationController
       pr.save
 
       @pr = pr.id
-      return
+    else
+      redirect_to '/'
     end
 
     print("\nnono")
@@ -26,11 +28,17 @@ class ResponseController < ApplicationController
 
   def disagree
     key = params[:key]
-
+    @key = key
+    @pr = ""
     if PersonResponse.where(disagree_hash: key).count != 0
       pr = PersonResponse.where(disagree_hash: key)[0]
       pr.response_type = "반대"
       pr.save
+
+      @pr = pr.id
+      return
+    else
+      redirect_to '/'
       return
     end
   end
@@ -40,16 +48,24 @@ class ResponseController < ApplicationController
         p = PersonResponse.where(agree_hash: params[:key])[0]
         p.response_text = params[:content]
         p.save
+
+        @c = p.response_text
+        @t = params
+        return
       end
     elsif params[:type] == "disagee"
       if PersonResponse.where(disagree_hash: params[:key]).count != 0
         p = PersonResponse.where(disagree_hash: params[:key])[0]
         p.response_text = params[:content]
         p.save
+        @c = p.response_text
+        @t = params
+        return
       end
     end
+    @c = "노노"
+    @t = "놉 ㅠㅠ"
 
-    redirect_to '/'
   end
 
 end
