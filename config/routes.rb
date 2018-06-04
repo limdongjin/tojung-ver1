75917957a32aa689c9c958b6a63e88f581c15c4c
Zinyon <jy2045@sogang.ac.kr>
2018-06-04 16:24:54 +0900

    footer 추가 / 이메일 placeholder 수정

--- config/routes.rb
+++ config/routes.rb
@@ -168,6 +168,8 @@ Rails.application.routes.draw do
   get 'home/index'
 
   get 'home/about'
+  get 'home/policy'
+  get 'home/privacy'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
